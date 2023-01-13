4c951048a0fb1cb8221ef74d8d732f31d1ec0e3b
unknown <songchengg23@gmail.com>
2023-01-13 17:25:12 +0800

    refresh

--- components/QLinkFunc.js
+++ components/QLinkFunc.js
@@ -6,9 +6,6 @@ const getQLinkNode = function ({title, url, icon}) {
 
     const node_actionMenu = document.createElement("button");
     node_actionMenu.setAttribute("class", "actionmenu-trigger");
-    const node_mask = document.createElement("div");
-    node_mask.setAttribute("class", "mask");
-    node_actionMenu.appendChild(node_mask);
 
     const node_iconContainer = document.createElement("div");
     node_iconContainer.setAttribute("class", "iconContainer");
@@ -30,29 +27,4 @@ const getQLinkNode = function ({title, url, icon}) {
 
     return node_QLink;
 }
-
-
-const ADDSHORTCUT = document.createElement("button");
-ADDSHORTCUT.setAttribute("id", "addshortcut");
-ADDSHORTCUT.setAttribute("title", "add shortcut");
-
-const node_iconContainer = document.createElement("div");
-node_iconContainer.setAttribute("class", "iconContainer");
-const node_icon = document.createElement("img");
-node_icon.setAttribute("id", "icon-addshortcut");
-node_icon.setAttribute("src", "../assets/addshortcut.png");
-node_iconContainer.appendChild(node_icon);
-
-const node_titleContainer = document.createElement("div");
-node_titleContainer.setAttribute("class", "titleContainer");
-const node_title = document.createElement("span");
-const textnode_title = document.createTextNode("addshortcut");
-node_title.appendChild(textnode_title);
-node_titleContainer.appendChild(node_title);
-
-ADDSHORTCUT.appendChild(node_iconContainer);
-ADDSHORTCUT.appendChild(node_titleContainer);
-
-
-
-export { getQLinkNode, ADDSHORTCUT };
+export { getQLinkNode };
