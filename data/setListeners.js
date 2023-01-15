ba9e892c788ef47d15089a17e52c5972b6d42b3a
unknown <songchengg23@gmail.com>
2023-01-15 18:49:30 +0800

    try

---
+++ data/setListeners.js
@@ -0,0 +1,229 @@
+// import
+import { QLink, Dialog } from "./classes.js"
+
+var activeQLink = null;
+
+
+
+// set addshortcut-dialog's listeners
+const dialog_addshortcut = document.getElementById("addshortcut-dialog");
+function setAddshortcutDialogListeners() {
+    dialog_addshortcut.addEventListener("click", (event) => {
+        event.stopPropagation();
+        openAddshortcutDialog();
+    })
+    
+    const cancle = dialog_addshortcut.querySelector(".dialog-cancle");
+    const done = dialog_addshortcut.querySelector(".dialog-done");
+    
+    done.addEventListener("click", (event) => {
+        event.stopPropagation();
+        const newShortcut = addShortcut();
+        setShortcutListener(newShortcut);
+        closeAddshortcutDialog();
+    })
+    cancle.addEventListener("click", (event) => {
+        event.stopPropagation();
+        closeAddshortcutDialog();
+    })
+}
+
+function openAddshortcutDialog() {
+    const input_name =  dialog_addshortcut.querySelector(".dialogInput-name");
+    const input_url = dialog_addshortcut.querySelector("dialogInput-url");
+
+    input_name.value = "";
+    input_url.value = "";
+    
+    dialog_addshortcut.style.display = "block";
+}
+
+function closeAddshortcutDialog(node) {
+    let dialog = node || dialog_addshortcut;
+
+    dialog.style.display = "none";
+}
+
+function addShortcut() {
+    const input_name =  dialog_addshortcut.querySelector(".dialogInput-name");
+    const input_url = dialog_addshortcut.querySelector(".dialogInput-url");
+
+    const name = input_name.value;
+    const url = input_url.value;
+
+    const node_QLinkList = document.getElementById("QLink-List");
+    const node_addshortcut = document.getElementById("addshortcut");
+
+    const newNode =  getQLinkNode(new QLink(name, url)); 
+    node_QLinkList.insertBefore(newNode, node_addshortcut);
+    return newNode;
+}
+
+// set shortcut's listeners
+function setShortcutListener(node) {
+    const actionmenuTrigger = node.querySelector(".actionmenu-trigger");
+    actionmenuTrigger.addEventListener("click", (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        activeQLink = event.target.parentNode;
+        openActionmenu(event);
+    });
+}
+
+// set action-menu's listeners
+const actionmemu = document.getElementById("actionmenu-dialog");
+function setActionmenuListeners() {
+    const edit = actionmemu.querySelector(".menu-item .edit");
+    const remove = actionmemu.querySelector(".menu-item .remove");
+    edit.addEventListener("click", (event) => {
+        openEditShortcutDialog(activeQLink);
+        closeActionmenu();
+    })
+    remove.addEventListener("click", (event) => {
+        removeShortcut(activeQLink);
+        closeActionmenu();
+        activeQLink = null;
+    })    
+}
+
+function openActionmenu(event) {
+    const trigger = event.target;
+    const QLink = trigger.parentNode;
+    const relativeX = QLink.offsetLeft;
+    const relativeY = QLink.offsetTop;
+
+    const actionmemu = document.getElementById("actionmenu-dialog");
+    actionmemu.style.display = "block";
+    actionmemu.style.top = relativeY + "px";
+    actionmemu.style.left = (relativeX - (actionmemu.offsetWidth - QLink.offsetWidth) / 2) + "px";
+}
+
+function closeActionmenu(node) {
+    let actionmemu = node || document.getElementById("actionmenu-dialog");
+    actionmemu.style.display = "none";
+}
+
+function removeShortcut(node) {
+    const QLinkList = document.getElementById("QLink-List");
+    QLinkList.remove(node);
+}
+
+function openEditShortcutDialog(node) {
+    const name = node.getAttribute("title");
+    const url = node.getAttribute("href");
+    
+    const dialog_editshortcut = document.getElementById("editshortcut-dialog");
+    const input_name =  dialog_editshortcut.querySelector(".dialogInput-name");
+    const input_url = dialog_editshortcut.querySelector(".dialogInput-url");
+
+    input_name.value = name;
+    input_url.value = url;
+}
+
+// set editshortcut-dialog's listeners
+const dialog_editshortcut = document.getElementById("editshortcut-dialog");
+function setEditshortcutDialogListeners() {
+    const cancle = dialog_editshortcut.querySelector(".dialog-cancle");
+    const done = dialog_editshortcut.querySelector(".dialog-done");
+    done.addEventListener("click", (event) => {
+        event.stopPropagation();
+        editShortcut(activeQLink);
+        closeEditshortcutDialog();
+    })
+    cancle.addEventListener("click", (event) => {
+        event.stopPropagation();
+        closeEditshortcutDialog();
+    })
+}
+
+function editShortcut(node) {
+    const input_name =  dialog_editshortcut.querySelector(".dialogInput-name");
+    const input_url = dialog_editshortcut.querySelector(".dialogInput-url");
+
+    const name = input_name.value;
+    const url = input_url.value;
+
+    node.setAttribute("title", name);
+    node.setAttribute("href", url);
+
+    const node_titleContainer = node.querySelector(".titleContainer");
+    const node_title = node_titleContainer.querySelector("span");
+    node_title.innerText = name;
+
+    const node_icon = node.querySelector(".icon-QLink");
+    node_icon.setAttribute("src", QLink.getFavicon(url));
+}
+
+function closeEditshortcutDialog(node) {
+    let dialog = node || dialog_editshortcut;
+
+    dialog.style.display = "none";
+}
+
+// set addshortcut-dialog's listeners
+const addshortcutTrigger = document.getElementById("addshortcut");
+function setAddshortcutTriggerListeners() {
+    addshortcutTrigger.addEventListener("click", (event) => {
+        event.stopPropagation();
+        openAddshortcutDialog();
+    });
+}
+
+// set body's listeners
+const body_ = document.body;
+
+function clickToCloseDialogListener() {
+    const dialogs = [];
+    dialogs.push(new Dialog("addshortcut-dialog", document.getElementById("addshortcut-dialog"), 
+    openAddshortcutDialog, closeAddshortcutDialog));
+    
+    dialogs.push(new Dialog("editshortcut-dialog", document.getElementById("editshortcut-dialog"),
+    openEditShortcutDialog, closeEditshortcutDialog));
+    
+    dialogs.push(new Dialog("actionmenu-dialog", document.getElementById("actionmenu-dialog"), 
+    openActionmenu, closeActionmenu));
+
+    body_.addEventListener("click", (event) => {
+        const target = event.target;
+
+        if (!isInDialog(target)) {
+            dialogs.forEach((dialog) => {
+                if (dialog.isOpen()) {
+                    dialog.close(dialog.node)
+                }
+            })
+        }
+    });
+}
+
+function setInitListeners() {
+    const node_QLinkList = document.getElementById("QLink-List");
+    const initQLinks = Array.from(node_QLinkList.children);
+
+    initQLinks.forEach((shortcut) => {
+        setShortcutListener(shortcut);
+    })
+}
+
+function setListeners() {
+    setInitListeners();
+
+    clickToCloseDialogListener();
+
+    setAddshortcutDialogListeners();
+    setEditshortcutDialogListeners();
+    setActionmenuListeners();
+    setAddshortcutTriggerListeners();
+}
+
+function isInDialog(node) {
+    if (node.tagName.toLowerCase() == "body") return false;
+
+    const classList = node.classList;
+    if (classList.contains("dialog")) return true;
+    else {
+        return isInDialog(node.parentNode);
+    }
+}
+
+export { setListeners };
